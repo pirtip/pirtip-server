d996af950b733cd415850308fe16b673c7c1af23
youngxpepp <youngxpepp@gmail.com>
2023-12-18 23:03:33 +0900

    init

---
+++ src/main/java/com/pirtip/pirtipserver/PirtipServerApplication.java
@@ -0,0 +1,13 @@
+package com.pirtip.pirtipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirtipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PirtipServerApplication.class, args);
+	}
+
+}
