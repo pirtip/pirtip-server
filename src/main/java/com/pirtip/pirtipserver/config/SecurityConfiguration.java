a1eaba48ed60310f97a257d43174e3b374f9ae1d
youngxpepp <youngxpepp@gmail.com>
2023-12-24 00:17:08 +0900

    feat: Spring Security 설정 추가

---
+++ src/main/java/com/pirtip/pirtipserver/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.pirtip.pirtipserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.formLogin().disable()
+			.httpBasic().disable();
+
+		http.authorizeRequests()
+			.antMatchers("/api/user/signup").anonymous()
+			.anyRequest().authenticated();
+
+		return http.build();
+	}
+}
