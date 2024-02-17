340762017118fe8c9d07f1c4c39dd5df1010c992
youngxpepp <youngxpepp@gmail.com>
2024-02-17 12:44:12 +0900

    feat: CORS 문제 해결

--- src/main/java/com/pirtip/pirtipserver/config/SecurityConfiguration.java
+++ src/main/java/com/pirtip/pirtipserver/config/SecurityConfiguration.java
@@ -1,12 +1,18 @@
 package com.pirtip.pirtipserver.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +24,33 @@ public class SecurityConfiguration {
 	}
 
 	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedOrigin("http://localhost:8080");
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedMethod(HttpMethod.GET);
+		configuration.addAllowedMethod(HttpMethod.HEAD);
+		configuration.addAllowedMethod(HttpMethod.POST);
+		configuration.addAllowedMethod(HttpMethod.PUT);
+		configuration.addAllowedMethod(HttpMethod.PATCH);
+		configuration.addAllowedMethod(HttpMethod.DELETE);
+		configuration.addAllowedMethod(HttpMethod.OPTIONS);
+		configuration.addAllowedMethod(HttpMethod.TRACE);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.formLogin().disable()
 			.httpBasic().disable();
 
 		http.formLogin();
+
+		http.cors();
 
 		http.authorizeRequests()
 			.antMatchers("/api/user/signup").anonymous()
