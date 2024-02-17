eb10144d95cb6e2e9532c0a39908e8477604a04d
youngxpepp <youngxpepp@gmail.com>
2024-02-17 12:03:11 +0900

    feat: swagger 서버 url 추가

---
+++ src/main/java/com/pirtip/pirtipserver/config/OpenApiConfiguration.java
@@ -0,0 +1,16 @@
+package com.pirtip.pirtipserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Configuration
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "https://yeop2.world/"),
+		@Server(url = "http://localhost:8080/")
+	}
+)
+public class OpenApiConfiguration {
+}
