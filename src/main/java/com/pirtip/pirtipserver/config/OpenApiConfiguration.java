2dc3fc27bcb7ec3f3e1f98d6dcb913e377b216c9
youngxpepp <youngxpepp@gmail.com>
2024-02-19 22:16:11 +0900

    feat: CORS 로컬 url 추가

--- src/main/java/com/pirtip/pirtipserver/config/OpenApiConfiguration.java
+++ src/main/java/com/pirtip/pirtipserver/config/OpenApiConfiguration.java
@@ -9,7 +9,8 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
 	servers = {
 		@Server(url = "https://yeop2.world/"),
-		@Server(url = "http://localhost:8080/")
+		@Server(url = "http://localhost:8080/"),
+		@Server(url = "http://192.168.0.41:8080/")
 	}
 )
 public class OpenApiConfiguration {
